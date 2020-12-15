2361aecbaec01fd262925d9fadcca47696dff6b0
Zandalor-DG <Paliy.zdg@gmail.com>
2020-12-15 20:32:22 +0300

    Swear I will never forget to push my daily changes to git and will never ask Alexey to do it instead of me

---
+++ backend_book_store/database/migrations/20201215133442-create-files.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Files', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      orignal_name: {
+        type: Sequelize.STRING
+      },
+      path_name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Files');
+  }
+};
---
+++ backend_book_store/middleware/multerUpload.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+exports.storageConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname + '-' + Date.now());
+  },
+});
+
+exports.fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
